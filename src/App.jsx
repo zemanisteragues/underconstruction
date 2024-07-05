bfb27886689fad2a15affcde746af4faeef07cd8
ZemanisterD <vimanyu.sharma@dizartsolutions.com>
2024-07-06 00:55:25 +0530

    image fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
         <img src={Construction} alt="Under Construction" className="w-full h-64" />
         <div className="p-4">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">GoodBuild</h1>
-          <p className="text-lg text-gray-600 mb-8">Our site is currently under upgradation. We'll be with you shortly.</p>
+          <p className="text-lg text-gray-600 mb-8">Our site is currently under upgradation. We will be with you shortly.</p>
           {/* <div className="mt-4">
             <input type="email" className="w-full p-2 border border-gray-300 rounded-lg" placeholder="Enter your email" />
             <button className="w-full mt-2 p-2 bg-blue-600 text-white rounded-lg">Notify Me</button>
